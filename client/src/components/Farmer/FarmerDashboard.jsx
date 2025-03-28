0c9e8481aa48d17d913336637f815ed709dd69cb
khaja <khajashek99@gmail.com>
2025-03-28 16:22:07 +0530

    first commit

--- client/src/components/Farmer/FarmerDashboard.jsx
+++ client/src/components/Farmer/FarmerDashboard.jsx
@@ -10,7 +10,6 @@ const FarmerDashboard = () => {
     { path: "/farmer/my-farms", icon: "🌾", title: "My Farms", description: "View and manage your registered farms." },
     { path: "/farmer/upload-document", icon: "📄", title: "Upload Document", description: "Submit necessary farm-related documents." },
     { path: "/farmer/my-documents", icon: "📂", title: "My Documents", description: "View and download uploaded documents." },
-    // { path: "/farmer/request-loan", icon: "💰", title: "Request a Loan", description: "Apply for farm-related financial support." },
     { path: "/farmer/my-loans", icon: "💳", title: "My Loans", description: "Track your loan requests and approvals." },
     { path: "/farmer/transactions", icon: "💸", title: "My Transactions", description: "Monitor all your financial transactions." },
     {path :"/common/report-issue", icon: "🚨", title: "Report Issue", description: "Report any issues or problems to the admin."},
